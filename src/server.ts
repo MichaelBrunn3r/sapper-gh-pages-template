0960918f8a3ce83fa3c7c7a4734dfc6aa8310b46
Michael Brunner <MichaelBrunn3r@gmail.com>
2020-12-11 21:36:18 +0100

    Added typescript support

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import * as sapper from '@sapper/server';
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-const url = dev ? '/' : '<REPONAME>';
+const url = dev ? '/' : '<TODO REPONAME>';
 
 polka() // You can also use Express
 	.use(
